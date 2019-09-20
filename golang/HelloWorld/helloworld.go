08ba78cf7e515a1168260d5f2071e6e0ae9d1cef
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-20 02:05:53 -0300

    Go - Hello world

--- golang/HelloWorld/helloworld.go
+++ golang/HelloWorld/helloworld.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+ reader := bufio.NewReader(os.Stdin)
+ fmt.Println("Hello, World.")
+ // The '_' represents a write-only value to be used as a place-holder
+ // where a variable is needed but the actual value is irrelevant.
+ // https://golang.org/doc/effective_go.html#blank
+ text, _ := reader.ReadString('\t')
+ fmt.Println(text)
 }
