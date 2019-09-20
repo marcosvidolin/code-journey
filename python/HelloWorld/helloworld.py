0d485e711695e8ad799902f44cc4e02f736efa7a
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-20 02:07:05 -0300

    Python - Hello world

--- python/HelloWorld/helloworld.py
+++ python/HelloWorld/helloworld.py
@@ -1,1 +1,5 @@
-print('Hello, world')
+inputString = input() # get a line of input from stdin and save it to our variable
+
+print('Hello, World.')
+
+print(inputString)
--- python/README.md
+++ python/README.md
@@ -5,3 +5,4 @@
 ## How to run
 
 ## References
+[Awesome Python](https://github.com/vinta/awesome-python)
