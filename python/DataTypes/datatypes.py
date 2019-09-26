ad8dc02af0ce85ef575a345552bb22e9644f92ec
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-26 01:48:58 -0300

    Python - Data types

---
+++ python/DataTypes/datatypes.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+second_i = 0
+second_d = 0.0
+second_s = ' '
+
+# Read and save an integer, double, and String to your variables.
+second_i = int(input())
+second_d = float(input())
+second_s = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + second_i)
+
+# Print the sum of the double variables on a new line.
+print(d + second_d)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + second_s)
